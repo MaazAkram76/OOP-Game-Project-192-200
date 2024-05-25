cecbc346b03a64e664fac88262fce89aa39d1234
Maaz_Akram <muhammadmaazsaat@gmail.com>
2024-05-25 12:01:19 +0500

    Just changed FPS from 55 to 66

--- OOPGameProject/OOPGameProject/main.cpp
+++ OOPGameProject/OOPGameProject/main.cpp
@@ -9,7 +9,7 @@ int main()
 
 	InitWindow(windowWidth, windowHeight, "231192 and 231200 OOP Game Project"); //initializing window
 
-	SetTargetFPS(55); //setting Game frame rate, incase we don't define the frame rate for the game,
+	SetTargetFPS(60); //setting Game frame rate, incase we don't define the frame rate for the game,
 					//computer will try to run it as fast as possible depending upon the computer's speed
 					//By defining the game frame rate we are ensuring that game will run at the same speed on every computer
 					//settting up the frame rate at 60 fps, we are allowing the game 'while' loop to UPDATE the GAME DISPLAY 60 times in a second
